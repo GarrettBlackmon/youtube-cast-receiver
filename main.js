34737a8817e242c2a418feb7c7da0852d5b50216
Garrett Blackmon <garrett@yellowcard.io>
2022-02-15 12:09:09 -0600

    testing build script

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const iconPath = path.join(__dirname, "build", "icon.png");
 function createWindow () {
     win = new BrowserWindow({fullscreen: true, icon: iconPath});
     win.loadURL('http://youtube.com/tv',
-      {userAgent: 'Mozilla/5.0 (Linux; Tizen 2.3) AppleWebKit/538.1 (KHTML, like Gecko)Version/2.3 TV Safari/538.1'});
+      {userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36; Youtube ; Tizen 4.0'});
  
     win.on('closed', () => {
       win = null
