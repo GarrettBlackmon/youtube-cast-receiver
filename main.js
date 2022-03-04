358c328033e449697ecd3c86f2c53cb595e48536
Garrett Blackmon <garrett@yellowcard.io>
2022-03-03 22:08:54 -0600

    disabled menubar for windows

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const path = require('path');
 const iconPath = path.join(__dirname, "build", "icon.png");
 
 function createWindow () {
-    win = new BrowserWindow({fullscreen: true, icon: iconPath});
+    win = new BrowserWindow(
+      {
+        fullscreen: true,
+        icon: iconPath,
+        autoHideMenuBar: true
+      });
     win.loadURL('http://youtube.com/tv',
       {userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36; Youtube ; Tizen 4.0'});
  
